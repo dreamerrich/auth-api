f65d917646a29e1d34ab0808c21378f00ffb5953
richi <richidhimar45@gmail.com>
2022-11-10 15:25:53 +0530

    did the viewset

--- demoApi/api/urls.py
+++ demoApi/api/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from django.urls import path
-from .views import profileView
+from django.urls import path, include
+from .views import profileViewSet
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register('api', views.profileViewSet,basename='profileapi')
+# router.register(r'api/<pk>', views.profileViewSet,basename='api')
 
 urlpatterns = [
-    path('api', profileView.as_view()),
+    # path('api', profileView.as_view()),
+    path('', include(router.urls)),
 ]
--- demoApi/api/views.py
+++ demoApi/api/views.py
@@ -10,7 +10,25 @@ from rest_framework import viewsets
 
 # --------------- get and post api ----------------
 
-class profileView(APIView):
+# class profileView(APIView):
+#     def post(self, request):
+#         serializer = profileSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#         else:
+#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+#     def get(self,request):
+#         data = profile.objects.all()
+#         serializers = profileSerializer(data, many=True)
+#         return Response(serializers.data, status=200)
+
+# -------------------- viewset -----------------
+
+class profileViewSet(viewsets.ViewSet):
+
     def post(self, request):
         serializer = profileSerializer(data=request.data)
         if serializer.is_valid():
@@ -19,15 +37,41 @@ class profileView(APIView):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def list(self, request):
+        p = profile.objects.all()
+        serializer = profileSerializer(p, many=True)
+        return Response(serializer.data)
 
-    def get(self,request):
-        data = profile.objects.all()
-        serializers = profileSerializer(data, many=True)
-        return Response(serializers.data, status=200)
+    def retrieve(self, request, pk):
+        id = pk
+        print("🚀 ~ file: views.py ~ line 47 ~ id", id)
+        if pk is not None:
+          p = profile.objects.get(id=pk)
+          print("🚀 ~ file: views.py ~ line 25 ~ P", p)
+          serializer = profileSerializer(p)
+          return Response(serializer.data)
 
-# -------------------- viewset -----------------
+    def update(self, request, pk):
+        id = pk
+        print("🚀 ~ file: views.py ~ line 56 ~ id", id)
+        p = profile.objects.get(id=pk)
+        serializer = profileSerializer(p, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message':'Data updated'})
+        return Response({'message':'Data created'}, status=status.HTTP_400_BAD_REQUEST)
 
-class profileViewset(viewsets.ViewSet):
-
-    def post(self, request):
-        pass
+    def partial_update(self, request, pk):
+        id = pk
+        p = profile.objects.get(id=pk)
+        serializer = profileSerializer(p, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message':'Data updated'})
+        return Response(serializer.errors)
+    
+    def destroy(self, request, pk):
+        id = pk 
+        p = profile.objects.get(id=pk)
+        p.delete()
+        return Response({'msg':'data deleted'})
